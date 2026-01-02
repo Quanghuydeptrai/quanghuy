a6a77d0641287a4936b29ac9c21321ce9c4011e0
joonwuy <huybanhton123@gmail.com>
2026-01-02 18:27:20 +0700

    Update version number in htools.py

--- htools.py
+++ htools.py
@@ -58,7 +58,7 @@ class UI:
         print(colored_logo + UI.RESET)
         print(f"{UI.BOLD}{UI.WHITE}──────────────────────────────────────────────────────────{UI.RESET}")
         print(f"{UI.YELLOW} Author   : {UI.WHITE}H-Tool 2026")
-        print(f"{UI.YELLOW} Update   : {UI.GREEN}Version 6.6 - Follow1s.Net{UI.RESET}")
+        print(f"{UI.YELLOW} Update   : {UI.GREEN}Version 4.1 - Follow1s.Net{UI.RESET}")
         print(f"{UI.BOLD}{UI.WHITE}──────────────────────────────────────────────────────────{UI.RESET}\n")
 
     @staticmethod
